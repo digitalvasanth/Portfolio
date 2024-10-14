d3b16a35eb290e8deb6f8134f295e45cb8a6acb5
K RAEKSH <rakeshkosireddy6@gmail.com>
2024-10-15 00:31:20 +0530

    edited as full stack developer

--- src/Components/Home/Intro.js
+++ src/Components/Home/Intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
     return (
         <div className='lg:absolute bg-gradient-to-r from-white rounded-2xl w-full lg:w-4/12   lg:mx-20 p-9'>
             <h1 className='mt-16 my-4  lg:my-4 text-5xl lg:text-7xl font-extralight'><span className='text-red-600'>Hi,</span> I'm Rakesh</h1>
-            <p className='text-xl my-2 lg:text-3xl lg:my-5'>A Front-end developer</p>
+            <p className='text-xl my-2 lg:text-3xl lg:my-5'>A Full Stack developer</p>
             <span className='text-sm lg:text-base'>I enjoy solving complex problems. Frequently praised as detail-oriented by my peers, I can be relied upon to help your company achieve its goals by providing sustainable and scalable solutions.</span>
             <div className='flex my-5 '>
                 {SOCIAL_MEDIA_LOGOS.map(logo => <a key={logo.id} href={logo.refer}><img className='w-7 h-7 lg:w-10 lg:h-10 mx-5 hover:cursor-pointer' alt={logo.logoName} src={logo.url} /></a>)}
