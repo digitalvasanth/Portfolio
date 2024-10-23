df9c15c6eaeebc6b72a2f1d10c8b52a6e54ef12c
K RAEKSH <rakeshkosireddy6@gmail.com>
2024-10-23 22:58:30 +0530

    created top-bottom nav icons

--- src/Components/Home/Body.js
+++ src/Components/Home/Body.js
@@ -3,10 +3,12 @@ import Intro from './Intro';
 import { BG_IMG } from '../../utils/constants';
 import { PROFILE_PIC } from '../../utils/constants';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-scroll';
 const Body = () => {
     const mode = useSelector(store => store.mode.isModeDark)
     let opacity = "opacity-95";
     let bodyBg = "";
+
 
     mode ? bodyBg = " bg-gray-800 " : bodyBg = "";
     mode ? opacity = "opacity-55" : opacity = "opacity-95";
@@ -18,14 +20,26 @@ const Body = () => {
                     <div className='lg:w-6/12 flex items-center'>
                         <Intro />
                     </div>
-
+                    <Link to="sectionTech"
+                        spy={true}
+                        smooth={true}
+                        duration={500}>
+                        <div className='lg:hidden   flex justify-center text-center -mt-10'><div className='bg-white w-12 h-12 rounded-full font-bold text-4xl'>&#8964;</div></div></Link>
                     <div className='lg:w-7/12 flex items-center  lg:pl-72'>
                         <img alt='profile'
                             className=' object-cover relative mt-14'
                             src={PROFILE_PIC} />
+
                     </div>
+
                 </div>
+                <Link to="sectionTech"
+                    spy={true}
+                    smooth={true}
+                    duration={500}>
+                    <div className='animate-ping hidden  lg:flex lg:justify-center text-center -mt-12'><div className='z-10 animate-bounce hover:bg-slate-400 bg-white w-14 h-14 rounded-full font-bold text-4xl'>&#8964;</div></div></Link>
             </div>
+
         </div>
 
     );
--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -5,7 +5,7 @@ const Navigator = () => {
     const [backToTop, setBackToTop] = useState(false);
     useEffect(() => {
         window.addEventListener("scroll", () => {
-            if (window.scrollY > 50)
+            if (window.scrollY > 200)
                 setBackToTop(true);
             else
                 setBackToTop(false)
@@ -19,7 +19,7 @@ const Navigator = () => {
                     smooth={true}
                     duration={500}>
                     <div className="lg:-mt-20 -mt-16 lg:mr-10 mr-3 text-center animate-pulse bg-lime-500 text-4xl pt-1 lg:pt-3 h-10 w-10 lg:h-14 lg:w-14 rounded-full">
-                        <img className=" " src="https://cdn-icons-png.flaticon.com/512/44/44603.png" />
+                        <img alt="top" src="https://cdn-icons-png.flaticon.com/512/44/44603.png" />
                     </div></Link>
             </div>
         );
